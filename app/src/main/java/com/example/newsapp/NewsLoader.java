d8de9f2a92758f2b36e82290c260691498e09656
rahulneal <takrahul1440@gmail.com>
2021-04-12 00:55:31 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/newsapp/NewsLoader.java
@@ -0,0 +1,35 @@
+package com.example.newsapp;
+
+
+//package com.example.moham.newsapp;
+
+        import android.content.AsyncTaskLoader;
+        import android.content.Context;
+
+        import java.util.List;
+
+/**
+ * Created by moham on 10/2/2017.
+ */
+
+public class NewsLoader extends AsyncTaskLoader<List<News>> {
+    private String mUrl;
+
+    public NewsLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    public List<News> loadInBackground() {
+        if(mUrl == null) {
+            return null;
+        }
+        return QueryUtils.fetchNewsData(mUrl);
+    }
+}
